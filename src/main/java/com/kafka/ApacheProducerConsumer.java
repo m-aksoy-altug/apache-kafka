666bbe4023a78019486511358a8d96a292561562
m-aksoy-altug <m.aksoy.altug@gmail.com>
2025-02-05 18:16:12 +1100

    Apache Producer and Stream sample

--- src/main/java/com/kafka/ApacheProducerConsumer.java
+++ src/main/java/com/kafka/ApacheProducerConsumer.java
@@ -4,28 +4,74 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Properties;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class ApacheProducerConsumer {
-
 	private static final Logger LOG = LoggerFactory.getLogger(ApacheProducerConsumer.class);
-
-
 	private static final String KAFKA_SERVER = "192.168.1.113:9094";
 	private static final String KAFKA_TOPIC = "stream-test";
+	private static final String KAFKA_STREAM_ID = "stream-analysis-test";
+	private static final String KAFKA_TOPIC_NEW = "stream-test-filtered";
 
 	public static void main(String[] args) {
 		// clean install compile exec:java -Dexec.mainClass="com.kafka.ApacheProducerConsumer"
-		try (final KafkaProducer<String, String> producer = setProducer();){
+		 ExecutorService executorService = Executors.newFixedThreadPool(2);		 
+         executorService.submit(ApacheProducerConsumer::producerStart);
+         executorService.submit(ApacheProducerConsumer::streamStart);
+         executorService.shutdown();
+	}
+	
+	private static void streamStart() {
+		LOG.info("Stream started on thread: " + Thread.currentThread().getName());
+		Properties propsStream= setStream();
+			StreamsBuilder strmBuldr= new StreamsBuilder();
+			KStream<String,String> streamData = strmBuldr.stream(KAFKA_TOPIC);
+			streamData
+	          .filter((k, v) -> k != null && k.trim().contains("Cust0"))
+	          .to(KAFKA_TOPIC_NEW);
+			Topology topology = strmBuldr.build();
+		try(KafkaStreams stream=new KafkaStreams(topology, propsStream);) {	
+			stream.start();
+			Thread.sleep(5000);
+		}catch(Exception e) {
+			LOG.error(e.getMessage());
+		}
+	}
+	
+	private static Properties setStream() {
+		Properties props = new Properties();
+		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG,KAFKA_STREAM_ID);
+		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,Serdes.String().getClass().getName());
+		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,Serdes.String().getClass().getName());
+		return props;
+	}
+	
+	private static void producerStart() {
+		LOG.info("Producer started on thread: " + Thread.currentThread().getName());
+		Properties props =setProducer();
+		try (final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);){	
+			// producer.initTransactions(); // Transactional producer
 			int x = 0;
 			do {
+				// producer.beginTransaction();
 				StringBuilder strbld= new StringBuilder();
 				String customerId =GenerateData.generateCustomerId();
 				strbld.append(customerId).append(" ")
@@ -38,32 +84,28 @@ public class ApacheProducerConsumer {
 				x++;
 				final ProducerRecord<String,String> producerRecord= 
 						new ProducerRecord<String, String>(KAFKA_TOPIC, customerId,strbld.toString());
-						producer.send(producerRecord);	
+				Future<RecordMetadata> ftrMetaData= producer.send(producerRecord);
+				LOG.info("RecordMetaData:"+ftrMetaData.isDone());
 			} while (x < 1000);
+			//producer.commitTransaction();
 		}catch(Exception e) {
 			LOG.error(e.getMessage());
 		}
-		
 	}
-
-	private static KafkaProducer<String, String> setProducer() {
+	
+	
+	private static Properties setProducer() {
 		Properties props = new Properties();
-		// host and port of brokers in the Kafka cluster
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
-		// Setting kafka topic 
 		props.put(ProducerConfig.CLIENT_ID_CONFIG, KAFKA_TOPIC);
-		// Setting class for key part of the message
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-		// Setting class for value part of the message
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-//		// Setting size of the batch of messages published in one produces
-//		props.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384"); // default 16Mb
-		// Ensuring full commit of all records in one produce
-		props.put(ProducerConfig.ACKS_CONFIG, "all"); //all
+		//props.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384"); // default 16Mb
+		props.put(ProducerConfig.ACKS_CONFIG, "all"); // default all
 		props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "5000"); // default 120_000. 2mins
 		props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "3000"); // default 30000, 30s
-		props.put("log-level", "DEBUG"); 
-	return new KafkaProducer<String, String>(props);
+		//props.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "my-transactional-id");  // Transactional producer
+	return props;
 	}
 }
 
@@ -76,7 +118,7 @@ class GenerateData {
 
 	public static String generateTimestamp() {
 		LocalDateTime startDate = LocalDateTime.of(2025, 2, 5, 14, 0, 0);
-		long randomSeconds = random.nextInt(365 * 24 * 60 * 60); // Random seconds within a year
+		long randomSeconds = random.nextInt(365 * 24 * 60 * 60); 
 		LocalDateTime randomDate = startDate.plusSeconds(randomSeconds);
 		return randomDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss:000'Z'"));
 	}
@@ -98,4 +140,5 @@ class GenerateData {
 		String[] urls = { "http://dummy.com/Acc11", "http://dummy.com/Mob11" };
 		return urls[random.nextInt(urls.length)];
 	}
+	
 }
